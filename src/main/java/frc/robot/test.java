c0549e51d18dc7cc332c0751e1c23c6686c2ea68
Nekarone <yifei_yan@asl.org>
2023-11-13 17:28:53 +0000

    added season2023

---
+++ src/main/java/frc/robot/test.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class test {
+
+    /**
+     * This is a test method that
+     * 
+     * @param num hint
+     * @return returning Hello World
+     */
+    public String testMethod(int num) {
+        return "Hello World";
+    }
+}
